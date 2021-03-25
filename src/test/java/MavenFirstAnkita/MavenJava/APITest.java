a33513aa6a329c4c171c915b002dddbb0d5f141a
AnkitaDNaik <ankitadnaik@gmail.com>
2021-03-25 19:47:01 +0530

    first commit

---
+++ src/test/java/MavenFirstAnkita/MavenJava/APITest.java
@@ -0,0 +1,12 @@
+package MavenFirstAnkita.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	@Test
+	public void Api()
+	{
+	System.out.println("API");
+	
+	}
+}
---
+++ src/test/java/MavenFirstAnkita/MavenJava/SeleniumTest.java
@@ -0,0 +1,14 @@
+package MavenFirstAnkita.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+@Test
+
+public void SCR()
+{
+System.out.println("Hat");
+
+}
+
+}
